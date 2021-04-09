0780e2e8b09d0dff1bdf03f21a6487724ba6f9c4
janelletait <65558755+janelletait@users.noreply.github.com>
2021-04-09 00:00:55 -0400

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,4 +1,6 @@
 from common import *
+from pickle import FALSE, TRUE
+from startA3.common import RTPacket
 
 class Node:
     def __init__(self, ID, networksimulator, costs):
@@ -6,22 +8,110 @@ class Node:
         self.ns = networksimulator
         num = self.ns.NUM_NODES        
         self.distanceTable = [[999 for i in range(num)] for j in range(num)]
-        self.routes = [0 for i in range(num)]
+        self.routes = [999 for i in range(num)]
 
         # you implement the rest of constructor
         for i in range(num):
-            self.distanceTable[ID][i] = costs[i];
+            self.distanceTable[ID][i] = costs[i]
             
         for i in range(num):
-            self.distanceTable[i][i] = 0;
+            self.distanceTable[i][i] = 0
+            
+        
 
     def recvUpdate(self, pkt):
         
         self.distanceTable[pkt.sourceid] = pkt.mincosts
         
         # you implement the rest of it  
+
+        for i in range(self.num):
+            if (pkt.mincosts[i] < self.distanceTable[pkt.sourceid][i]):
+                self.distanceTable[pkt.sourceid][i] = pkt.mincosts[i]
+                
+        #update all nodes using bellman ford algorithm
+        
+
+        #int x; // temp variable used to detect changes in nodeID row
+        changed = FALSE
+
+
+        for r in range(self.num):
+            for c in range(self.num):
+
+                x = self.distanceTable[r][c]    #initial value in table
+
+                self.bellmanford(self, r, c)   #try to update it with the bellman ford algorithm
+
+                if (x != self.dataTable[r][c]):  #if this has changed, and we are in row of current node, update changed boolean
+                    changed = TRUE
+                    
+                
+        for r in range(self.num):
+            for c in range(self.num):
+
+
+                if (self.distanceTable[r][c] < self.distanceTable[c][r]):    #because bi-directional consider diagonal reflection
+                    self.distanceTable[c][r] = self.distanceTable[r][c]
+                    changed = TRUE
+                    
+                if (self.distanceTable[c][r] < self.distanceTable[r][c]):   #now reverse
+                    self.distanceTable[r][c] = self.distanceTable[c][r]
+                    changed = TRUE
+                    
+                if (r == c):                  #if we send to self, cost is 0
+                    if (self.distanceTable[r][c] != 0):
+                        changed = TRUE
+                        
+                    self.distanceTable[r][c] = 0
+                    
+
+                    
+            
+
+
+            #only need to send to neighbors if we have updated shortest path
+        if (changed):
+            self.toNeighbors(self)
+            
               
-        return 
+            return;
+        
+        
+    def toNeighbors(self):
+
+        newpkt = RTPacket()
+        
+        #source ID = node you send from = this node ID
+        newpkt.sourceid = self.myID;
+
+        #initialize data to row of this node ID
+        for i in range(self.num):
+            newpkt.mincosts[i] = self.distanceTable[self.myID][i]
+            
+
+
+        #send state to reachable neighbors
+        for i in range(self.num):
+            if (i != self.myID and self.routes[i] < 999):  #don't send to self or unreachable nodes
+                newpkt.destid = i
+                self.ns.toLayer2(self, newpkt)
+                
+                
+    def bellmanford(self, source, dest):
+        costs = []
+
+        for i in range(self.num):
+            costs[i] = self.distanceTable[source][i] + self.distanceTable[i][dest]
+            
+        calcMin = min(costs)
+
+        if (calcMin < self.distanceTable[source][dest]):
+            self.distanceTable[source][dest] = calcMin
+            
+
+
+        
 
     
     def printdt(self):
